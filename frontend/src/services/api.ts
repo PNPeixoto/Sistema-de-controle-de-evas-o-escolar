cdb2f34f2ab37e1621ff6f15f6d72b29caf3896e
Pedro Peixoto <phnpeixoto07@gmail.com>
2026-03-29 01:00:59 -0300

    vercel

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // O segredo está aqui: o "export const api" permite usar import { api } nos outros arquivos
 export const api = axios.create({
-    baseURL: 'http://localhost:8080', // Confirme se essa é a porta do seu Spring Boot
+    baseURL: 'https://sistema-pnp.onrender.com',
 });
 
 // Interceptor: Injeta o Token JWT em TODAS as requisições automaticamente
